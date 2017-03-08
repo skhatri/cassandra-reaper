471fcfac3e8db50b12652b4f79d32ec93e8cc47e
Alexander Dejanovski <alex@thelastpickle.com>
2017-03-08 07:21:07 +0100

    Add heartbeat system Adjust pause time to match intensity when several instances are running

--- src/main/java/com/spotify/reaper/service/SegmentRunner.java
+++ src/main/java/com/spotify/reaper/service/SegmentRunner.java
@@ -543,7 +543,7 @@ private void abort(RepairSegment segment, JmxProxy jmxConnection) {
       long repairDuration = repairEnd - repairStart;
       long delay = (long) (repairDuration / intensity - repairDuration);
       LOG.debug("Scheduling next runner run() with delay {} ms", delay);
-      return delay;
+      return delay*context.storage.countRunningReapers();
     } else {
       LOG.error("Segment {} returned with startTime {} and endTime {}. This should not happen."
                 + "Intensity cannot apply, so next run will start immediately.",
